138137eba48f01d152a053870e911e5de6eb8e61
Kuptsevich Boris <esrb2000@bk.ru>
2023-01-19 16:31:42 +0700

    modifed main

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Processor processor = new Processor(3.2, 2, "AMD", 0.25);
+        Ram ram = new Ram();
+        HardDrive hardDrive = new HardDrive();
+        Monitor monitor = new Monitor();
+        Keyboard keyboard = new Keyboard();
+        Computer computer = new Computer("intel", "x5", processor, ram, hardDrive, monitor, keyboard);
+        System.out.println(computer.getTotalWeigth());
 
     }
 }
